a0db32d301580df4919e0b3d90f7e16288a42b23
Susan Rotondo <susanrotondo@gmail.com>
2016-05-14 21:36:40 -0700

    skeleton JS and jQuery for creating grid of card div's

--- js/app.js
+++ js/app.js
@@ -1,1 +1,36 @@
+var mode = 'easy';
+var numColors = 0;
+var cardGrid = [];
 
+if(mode === 'easy') {
+  numColors = 2;
+}
+
+function deal() {
+  for(var i = 1; i <= 9; i++) {
+    cardGrid.push(new Card(i));
+    cardGrid.push(new Card(i));
+  }
+  return cardGrid;
+}
+
+function Card(value) {
+  this.innerText = value;
+}
+
+var $play = $('#play-button');
+var $cardContainer = $('#card-container')
+
+$play.on('click',function() {
+  var grid = deal();
+  $.each(grid, function(index, value) {
+    $cardContainer.append('<div class="card">' + value.innerText + '</div>');
+  });
+});
+
+
+
+///////////////////////////
+//     UNIT TESTING
+///////////////////////////
+// console.log(deal());
